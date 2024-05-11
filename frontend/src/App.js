f69174f83cd9375eabf564f732a132434239d7c5
razzivofficial <razzivofficial@gmail.com>
2024-05-11 21:54:44 +0530

    Homepage-navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
     </>
