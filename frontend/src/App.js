cea994416cc17fc1adc5ad278efc737928d4869b
Tushar Gandhi <gandhitushar509@gamail.com>
2024-05-12 04:16:14 +0530

    chatbot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
+import Product from './Pages/Product/Product';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<Homepage/>} />
         </Routes>
       </BrowserRouter>
     </>
