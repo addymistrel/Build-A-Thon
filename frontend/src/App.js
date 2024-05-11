d661defffc706ed0ccfacb835dc47e73a8695a64
Abhinav Singh <kittusinghranchi@gmail.com>
2024-05-12 01:48:34 +0530

    nurse registration route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
+import NurseRegistration from "./Pages/NurseRegistration/NurseRegistration";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/nurse/registration" element={<NurseRegistration/>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/Pages/NurseRegistration/NurseRegistration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NurseRegistration = () => {
+  return (
+    <div>
+      Nurse registration here
+    </div>
+  )
+}
+
+export default NurseRegistration
