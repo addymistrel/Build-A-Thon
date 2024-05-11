09a6b0feaeafa4a13a0803568debdfac4be287d8
Abhinav Singh <kittusinghranchi@gmail.com>
2024-05-12 02:41:02 +0530

    activeNurseBooking page ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
 import NurseRegistration from "./Pages/NurseRegistration/NurseRegistration";
+import NurseActiveBooking from "./Pages/NurseActiveBookings/NurseActiveBooking";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/nurse/registration" element={<NurseRegistration/>} />
+          <Route path="/nurse/active/bookings" element={<NurseActiveBooking/>} />
         </Routes>
       </BrowserRouter>
     </>
