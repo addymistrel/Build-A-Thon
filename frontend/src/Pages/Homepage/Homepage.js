8a6806a6a69f07a6b1d9186378a390deb634f5f7
razzivofficial <razzivofficial@gmail.com>
2024-05-12 12:10:31 +0530

    added-pricing-page

--- frontend/src/Pages/Homepage/Homepage.js
+++ frontend/src/Pages/Homepage/Homepage.js
@@ -6,13 +6,14 @@ import Footer from "../../Components/Footer/Footer";
 import Mainsection from "../../Components/Mainsection/Mainsection";
 import HeroSection from "../../Components/Herosection/Herosection";
 import Stats from "../../Components/Stats/Stats";
+import Pricing from "../../Components/Pricing/Pricing";
 
 export default function Homepage() {
   return (
     <>
-      
       <HeroSection />
       <Mainsection />
+      <Pricing />
       <Footer />
     </>
   );
